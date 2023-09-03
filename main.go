8ddef0de128fffa1ee20ebfeb88da53451a2255f
Jastria Rahmat <ijash1000@gmail.com>
2023-09-04 03:58:44 +0700

    remove dotenv

--- main.go
+++ main.go
@@ -1,29 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"ijash-jwt-auth/src/configs"
 	"ijash-jwt-auth/src/routes"
-	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	LoadEnv()
+
 	configs.InitDatabase()
 	e := echo.New()
 	routes.InitRoute(e)
 	e.Start(":8000")
 
-}
-
-func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	fmt.Printf("Environment: %s", os.Getenv("ENV"))
 }
