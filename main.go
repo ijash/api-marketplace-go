a390f698e4ff2d29e7f87b0bd4584850310f0312
Jastria Rahmat <ijash1000@gmail.com>
2023-09-04 05:27:22 +0700

    fix

--- main.go
+++ main.go
@@ -4,17 +4,27 @@ import (
 	"fmt"
 	"ijash-jwt-auth/src/configs"
 	"ijash-jwt-auth/src/routes"
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Println(os.Getenv("ENV"))
+	// LoadEnv()
 	configs.InitDatabase()
 	e := echo.New()
 	routes.InitRoute(e)
-
+	// e.Start(":8000")
 	e.Start(os.Getenv("PORT"))
 
+}
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fmt.Printf("Environment: %s", os.Getenv("ENV"))
 }
