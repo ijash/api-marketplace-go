964838c21600cf2f472f9ed073f9fc802e255371
Jastria Rahmat <ijash1000@gmail.com>
2023-09-04 05:57:32 +0700

    uuid

--- src/utils/uuid.go
+++ src/utils/uuid.go
@@ -1,19 +1,20 @@
 package utils
 
 import (
-	"log"
-	"os/exec"
-	"strings"
+	"crypto/rand"
+	"fmt"
 )
 
 func GenStringUUID() string {
-	cmd := exec.Command("uuidgen")
-	uuidBytes, err := cmd.Output()
+	uuid := make([]byte, 16)
+	_, err := rand.Read(uuid)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	// Convert the bytes to a string and remove leading/trailing whitespace
-	uuid := strings.TrimSpace(string(uuidBytes))
-	return uuid
+	// Set the version (4) and variant (2) bits
+	uuid[6] = (uuid[6] & 0x0F) | 0x40
+	uuid[8] = (uuid[8] & 0x3F) | 0x80
+
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:16])
 }
